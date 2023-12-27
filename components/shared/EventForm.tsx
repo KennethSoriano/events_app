00e7a785d60539ae3f0380f936a0f151ce524db1
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-12-27 11:44:40 -0800

    file uploader

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -10,6 +10,9 @@ import * as z from 'zod'
 import { eventDefaultValues } from "@/constants"
 import Dropdown from "./Dropdown"
 import { Textarea } from "@/components/ui/textarea"
+import {FileUploader} from "./FileUploader"
+import { useState } from "react"
+import Image from "next/image"
 
  
 
@@ -19,7 +22,7 @@ type EventFormProps = {
 }
 
 const EventForm = ({ userId, type} : EventFormProps) => {
-
+    const [files, setFiles] = useState<File[]>([])
     const initialValues = eventDefaultValues;
 
     const form = useForm<z.infer<typeof eventFormSchema>>({
@@ -92,9 +95,67 @@ const EventForm = ({ userId, type} : EventFormProps) => {
                         render={({ field }) => (
                             <FormItem className="w-full">
                                 <FormControl className="h-72">
-                                    
+                                    <FileUploader 
+                                        onFieldChange={field.onChange}
+                                        imageUrl={field.value}
+                                        setFiles={setFiles}
+                                    />
                                 </FormControl>
 
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="location"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl>
+                                    <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                                        <Image 
+                                            src='/assets/icons/location-grey.svg'
+                                            alt="calendar"
+                                            width={24}
+                                            height={24}
+                                        />
+                                        <Input placeholder="Event Location or Online" {...field} className="input-field"/>
+                                    </div>   
+                                </FormControl>
+                                <FormDescription>
+                                    This is your public display name.
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="startDateTime"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl>
+                                    <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                                        <Image 
+                                            src='/assets/icons/calendar.svg'
+                                            alt="calendar"
+                                            width={24}
+                                            height={24}
+                                            className="filter-grey"
+                                        />
+                                        <p className="ml-3 whitespace-nowrap text-grey-600">Start Date: </p>
+                                        <
+                                    </div>   
+                                </FormControl>
+                                <FormDescription>
+                                    This is your public display name.
+                                </FormDescription>
                                 <FormMessage />
                             </FormItem>
                         )}
